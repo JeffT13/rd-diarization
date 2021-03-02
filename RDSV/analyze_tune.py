6a81f97cd6fbfd9fd9e628524aff9ed06a49409a
JeffT13 <jefftumminia@gmail.com>
2021-03-01 20:21:44 -0500

    prints

--- RDSV/analyze_tune.py
+++ RDSV/analyze_tune.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy import stats
 from param import *
 
-avgder_thresh = .25
-
+avgder_thresh = .20
+hold = 100
 with open(embed_path) as je: 
     embed = json.load(je)
   
@@ -13,6 +13,7 @@ with open(tune_eval_path) as jt:
     
 for key in embed.keys():
     size = [i[2] for i in embed[key]]
+    print('\n\n\n\n\n')
     print('R=', key, '| avg size=', np.mean(size))
     
     run = tune[key]
@@ -20,8 +21,15 @@ for key in embed.keys():
         ral = run[1][r]
         ev = run[0][r]
         print('RAL settings:', r, '|', ral)
+        print()
         for set in ev.keys():
             temp = stats.describe(ev[set])
             if temp[2]<avgder_thresh:
                 #Mean, SD, Max
                 print(r,'-',set,':', temp[2], np.sqrt(temp[3]), temp[1][1])
+                if temp[2]<hold:
+                    hold=temp[2]
+                    id = key+'|'+r+'|'+set
+                    perm = temp
+                    
+print('\n\n\n\n', 'Best Param:', id, ' === ', perm)
