a51b27f4bed9eabc3469f7ebc575527bddcbf8d4
JeffT13 <jefftumminia@gmail.com>
2021-03-11 00:25:13 -0500

    typo

--- plots/rclone.sh
+++ plots/rclone.sh
@@ -3,26 +3,26 @@
 module load rclone/1.53.3
 
 
-rclone copy /scratch/jt2565/data/diarization/17-571_ral.rttm dgr:greene/info
-rclone copy /scratch/jt2565/data/diarization/17-571_rdsv.rttm dgr:greene/info
-rclone copy /scratch/jt2565/data/rttm/17-571.rttm dgr:greene/info
+rclone copy /scratch/jt2565/data/diarization/17-571_ral.rttm gdr:greene/info
+rclone copy /scratch/jt2565/data/diarization/17-571_rdsv.rttm gdr:greene/info
+rclone copy /scratch/jt2565/data/rttm/17-571.rttm gdr:greene/info
 
 
-rclone copy /scratch/jt2565/data/out/ral_spkr_dict.json dgr:greene/info
-rclone copy /scratch/jt2565/data/out/set_dict.json dgr:greene/info
-rclone copy /scratch/jt2565/data/out/lr_dict.json dgr:greene/info
-rclone copy /scratch/jt2565/data/out/test_eval.csv dgr:greene/info
+rclone copy /scratch/jt2565/data/out/ral_spkr_dict.json gdr:greene/info
+rclone copy /scratch/jt2565/data/out/set_dict.json gdr:greene/info
+rclone copy /scratch/jt2565/data/out/lr_dict.json gdr:greene/info
+rclone copy /scratch/jt2565/data/out/test_eval.csv gdr:greene/info
 
 
 
 
-rclone copy /scratch/jt2565/data/out/r1/X.npy dgr:greene/r1
-rclone copy /scratch/jt2565/data/out/r1/Xd.npy dgr:greene/r1
-rclone copy /scratch/jt2565/data/out/r1/Y.npy dgr:greene/r1
-rclone copy /scratch/jt2565/data/out/r1/Yd.npy dgr:greene/r1
+rclone copy /scratch/jt2565/data/out/r1/X.npy gdr:greene/r1
+rclone copy /scratch/jt2565/data/out/r1/Xd.npy gdr:greene/r1
+rclone copy /scratch/jt2565/data/out/r1/Y.npy gdr:greene/r1
+rclone copy /scratch/jt2565/data/out/r1/Yd.npy gdr:greene/r1
 
 
-rclone copy /scratch/jt2565/data/out/r5/X.npy dgr:greene/r5
-rclone copy /scratch/jt2565/data/out/r5/Xd.npy dgr:greene/r5
-rclone copy /scratch/jt2565/data/out/r5/Y.npy dgr:greene/r5
-rclone copy /scratch/jt2565/data/out/r5/Yd.npy dgr:greene/r5
+rclone copy /scratch/jt2565/data/out/r5/X.npy gdr:greene/r5
+rclone copy /scratch/jt2565/data/out/r5/Xd.npy gdr:greene/r5
+rclone copy /scratch/jt2565/data/out/r5/Y.npy gdr:greene/r5
+rclone copy /scratch/jt2565/data/out/r5/Yd.npy gdr:greene/r5
